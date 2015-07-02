afdd0ccf75653b92e1e30b058adb4ec1ba522465
KonstantinbBerkow <berkowk@gmail.com>
2015-07-02 14:27:52 +0300

    prepared rask 1

---
+++ lesson1task1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import random
+
+print 'Hello. I am a python program.'
+name = raw_input("What is your name? ")
+print "Hello there, " + name + ", random number is "
+print random.random()
