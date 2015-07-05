9567148d87ed73e242869d57ef1d0d642b925839
Konstantin <berkowk@gmail.com>
2015-07-05 15:01:23 +0300

    task7 fixed

--- task7.py
+++ task7.py
@@ -1,11 +1,18 @@
-import sys, math, random
+import math, random
 
-n = int(sys.argv[1])
+n = 0
 count = 0
-for i in range(0, n):
+prob = 2.0 / 6
+empProb = 0
+while True:
+	n += 1
 	x = random.randint(1, 6)
 	y = random.randint(1, 6)
 	if (x == 6 or y == 6):
 		count += 1
+	empProb = float(count) / n
+	if math.fabs(empProb - prob) < 1e-3:
+		break
 
-print float(count) / n
+print 'try count: %d' % n
+
